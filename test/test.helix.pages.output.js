c08140f6b2b0d0364d9e20621e6001ca5f94b954
Alexandre Capt <acapt@adobe.com>
2019-10-08 18:15:56 +0200

    Parametrize smoke tests

--- test/test.helix.pages.output.js
+++ test/test.helix.pages.output.js
@@ -21,7 +21,7 @@ const { JSDOM } = require('jsdom');
 const HTTP_REQUEST_TIMEOUT_MSEC = 10000;
 
 // TODO build this URL from the Git repo name - and support local test as well with hlx up
-const testURL = `https://helix-example-basic-adobe.project-helix.page/?cacheKiller=${Math.random()}`;
+const testURL = `https://helix-example-basic-adobe.${process.env.TEST_DOMAIN}/?cacheKiller=${Math.random()}`;
 
 // TODO we should first wait for the Helix Pages output to be
 // updated - include the Git revision hash in a response header
